3c188f3b57487045aa88b58088948b384ad74c9e
clairecoxo <cohec823@newschool.edu>
2018-10-31 16:46:34 -0400

    midterm

--- exercise_7/script.js
+++ exercise_7/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     
-    var originalText = "Native Americans were removed from the lands they had managed for centuries, not only during settlement, as is well known, but during the creation of the national parks and national forests. Indians resisted these moves in an effort to maintain autonomy and access to resources." ;
+    var originalText = "Native Americans were removed from the lands they had managed for centuries, not only during settlement, as is well known, but during the creation of the national parks and national forests. Indians resisted these moves in an effort to maintain autonomy and access to resources. —Shades of Darkness: Race and Enviromental History by Carolyn Merchent. Published July, 2003 Vol. 8 No. 3" ;
     var splitText = originalText.split(" ");
-    
-    
+    console.log(splitText.length)
+    var zindex = 0;  
     var ticker = 0;
     var backgrounds = ['background_1.jpg', 'background_2.jpg','background_3.jpg', 'background_4.jpg','background_5.jpg','background_6.jpg', 'background_7.jpg',]
     
@@ -13,16 +13,18 @@ $(document).ready(function(){
         $('body').css("background-image", "url(img/"+ randomBackground +")");  
 
         $(this).css("transform", "scale(9)")
-        $(this).css("z-index", 100+ticker)
         $(this).css("pointer-events", "none")
 
         $(this).html(splitText[ticker]);
         
         ticker += 1;
-        if(ticker>=splitText.length-1){
+        if(ticker>=splitText.length){
             ticker=0;
         }
         
+        zindex +=1;
+         $(this).css("z-index", 100+zindex)
+         
         
     })
     
