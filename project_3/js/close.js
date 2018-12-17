51bdd3c08630a3932caf730403e710174e95dfac
clairecoxo <cohec823@newschool.edu>
2018-12-16 22:30:07 -0500

    fixedwee

--- project_3/js/close.js
+++
@@ -1,7 +0,0 @@
-    window.onload = function(){
-    document.getElementById('close').onclick = function(){
-        this.parentNode.parentNode.parentNode
-        .removeChild(this.parentNode.parentNode);
-        return false;
-    };
-};
